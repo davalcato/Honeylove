25f4468ae08799d034f91683b2c4e082f1baab3f
davalcato <davalcato@yahoo.com>
2021-07-23 16:36:42 -0700

    first commit

---
+++ Honeylove/HoneyloveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyloveApp.swift
+//  Honeylove
+//
+//  Created by Daval Cato on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct HoneyloveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
