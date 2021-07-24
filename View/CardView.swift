0428006863b09108ca70c5b5a526da6dc6c2a8d2
davalcato <davalcato@yahoo.com>
2021-07-24 12:07:13 -0700

    Added a divider between the text and then created the second text view. And also added the capsule fill to the bottom text.

--- View/CardView.swift
+++ View/CardView.swift
@@ -17,6 +17,45 @@ struct CardView: View {
             .cornerRadius(24)
             .scaledToFit()
             .frame(minWidth: 0, maxWidth: .infinity)
+        // Honeylove place destination
+            .overlay(
+                VStack(alignment: .center,
+                       spacing: 12) {
+                Text(honeylove.place.uppercased())
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .shadow(radius: 1)
+                    .padding(.horizontal, 18)
+                    .padding(.vertical, 4)
+                    .overlay(
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(height:1),
+                        alignment: .bottom
+                    
+                    )
+                
+                Text(honeylove.country.uppercased())
+                    .foregroundColor(Color.black)
+                // Capsule shape under the text view
+                    .font(.footnote)
+                    .fontWeight(.bold)
+                    .frame(minWidth: 85)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical,5)
+                    .background(
+                        Capsule().fill(Color.white)
+                    
+                    
+                    
+                    )
+                
+            }
+                    .frame(minWidth: 280)
+                    .padding(.bottom, 50),
+                alignment: .bottom
+        )
     }
 }
 
